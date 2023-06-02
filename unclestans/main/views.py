b73a2b711eb0e6cf9e64b23d059a1204ee5a8ffb
bisiAlgoRuthM <bisibeauty@yahoo.com>
2023-06-02 14:58:07 -0500

    rendering templates and static files

--- unclestans/main/views.py
+++ unclestans/main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def order(request):
+    return render(request, 'main/order.html')
+
+def create_order(request):
+    if request.method == 'POST':
+        # Retrieve the data from the request
+        customer_name = request.POST.get('customer_name')
+        items = request.POST.get('items')
+        quantity = request.POST.get('quantity')
+
+        # Create a new order object
+        order = Order(customer_name=customer_name, items=items, quantity=quantity)
+        order.save()
+
+        # Return a JSON response indicating success
+        return JsonResponse({'status': 'success'})
+    else:
+        # Return an error response for invalid request method
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
