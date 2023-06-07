4e21cac81264908f84cf8e410b396b7f44a4b217
bisiAlgoRuthM <bisibeauty@yahoo.com>
2023-06-07 03:22:43 -0500

    updated models

--- unclestans/main/models.py
+++ unclestans/main/models.py
@@ -4,20 +4,19 @@ from django.db import models
 from django.db import models
 
 class OrderModel(models.Model):
-    customer_name = models.CharField(max_length=100)
     items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
     quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
     def __str__(self):
-        return f"Order #{self.id} - {self.customer_name} : {self.created_at.strftime('%b %d %I %M %p')}"
+        return f"Order #{self.id} - {self.created_at.strftime('%b %d %I %M %p')}"
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=3)
-    category = models.ForeignKey('Category',  on_delete=models.CASCADE, related_name='item', default=1)
+    category = models.ManyToManyField('Category', related_name='item')
 
     def __str__(self):
         return self.name
